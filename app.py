af084834d51ecd9e3e574a1b8152013b87655785
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2020-05-06 19:10:26 +0800

    Add Docker files

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def test_page():
     return render_template('test.html')
 
 if __name__=="__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80, debug=True)
---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="ngrq123/udacity-devops-capstone"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+cat ./my_password.txt | docker login --username ngrq123 --password-stdin
+docker tag udacity-devops-capstone $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
